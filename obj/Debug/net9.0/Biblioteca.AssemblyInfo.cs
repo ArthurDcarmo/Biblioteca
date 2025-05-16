ef4ca0deee4101e41f76c09461b4ef7709a248df
ArthurDcarmo <arthurconceicao47@gmail.com>
2025-05-15 22:46:22 -0300

    Adicionado Biblioteca inc

--- obj/Debug/net9.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net9.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ad4296812c75497fcd90bb82b538686a998350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e38897ba56b51445503a66b8e64ad783d00e55c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
